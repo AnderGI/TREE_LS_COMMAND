11549f361aaaed892fd30cc905a7cbb337d5cafe
AnderGI <agibarguen@gmail.com>
2023-07-25 08:47:25 +0200

    Refactoring: createInfoMssg function added

--- index.mjs
+++ index.mjs
@@ -4,23 +4,20 @@ import picocolors from "picocolors";
 
 async function tree(path = ".", indentation = "-") {
   try {
+    //get all the elements of the path
     const elements = await readdir(path);
     for (const element of elements) {
-      const elementPath = join(path, element); // Create the absolute path
+      //create the path of each element
+      const elementPath = join(path, element);
       const elementStat = await stat(elementPath);
-      const elementSymbol = elementStat.isDirectory() ? "D : " : "F : ";
-      const lastTimeModified = elementStat.mtime.toLocaleString();
-      const size = elementStat.size;
-      let infoMssg =
-        (elementSymbol === "D : "
-          ? picocolors.yellow(elementSymbol)
-          : picocolors.magenta(elementSymbol)) + " ";
-      infoMssg += picocolors.red(element).padEnd(20) + " ";
-      infoMssg += picocolors.green(size).padEnd(10) + " ";
-      infoMssg +=
-        "Última modificación : ".padStart(30) +
-        picocolors.cyan(lastTimeModified);
 
+      //create a custom ls command info message
+      const infoMssg = createInfoMssg({
+        element,
+        stats: elementStat,
+      });
+
+      //recursive function
       if (elementStat.isDirectory()) {
         console.log(indentation + infoMssg);
         await tree(elementPath, (indentation += "-"));
@@ -31,6 +28,21 @@ async function tree(path = ".", indentation = "-") {
   } catch (err) {
     console.log(err);
   }
+}
+
+function createInfoMssg({ element, stats }) {
+  const elementSymbol = stats.isDirectory() ? "D : " : "F : ";
+  const lastTimeModified = stats.mtime.toLocaleString();
+  const size = stats.size;
+  let infoMssg =
+    (elementSymbol === "D : "
+      ? picocolors.yellow(elementSymbol)
+      : picocolors.magenta(elementSymbol)) + " ";
+  infoMssg += picocolors.red(element).padEnd(20) + " ";
+  infoMssg += picocolors.green(size).padEnd(10) + " ";
+  infoMssg +=
+    "Última modificación : ".padStart(30) + picocolors.cyan(lastTimeModified);
+  return infoMssg;
 }
 
 tree();
